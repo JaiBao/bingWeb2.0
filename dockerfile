1c6bff7bfffd8811790db2f71b8399d40c35e5a2
baoshie <online988988@gmail.com>
2024-06-20 10:15:51 +0800

    new

---
+++ dockerfile
@@ -0,0 +1,32 @@
+# develop stage
+FROM node:18-alpine as develop-stage
+WORKDIR /app
+COPY package*.json ./
+# RUN npm i
+COPY . .
+
+ARG ENV_FILE
+ENV ENV_FILE=${ENV_FILE}
+
+RUN apk add --no-cache bash
+RUN cp /app/${ENV_FILE} /app/.env && npm i
+
+# build stage
+FROM develop-stage as build-stage
+RUN npm run build
+
+
+# # 创建 SSL 目录
+# COPY default.conf /etc/nginx/conf.d/default.conf
+# COPY nginx.conf /etc/nginx/nginx.conf
+# RUN mkdir -p /etc/nginx/ssl
+# production stage
+FROM node:18-alpine as production-stage
+WORKDIR /app
+COPY --from=build-stage /app/.output /app/.output
+EXPOSE 3000
+
+
+# 使用 node 運行構建項目
+ENTRYPOINT ["node", ".output/server/index.mjs"]
+
---
+++ pages/cico.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="deliciousTitle">
+    <img src="/delicious/cicoBannerApp.png" alt="banner" class="lt-md" />
+    <img src="/delicious/cicoBanner.png" alt="banner" class="gt-sm" />
+  </div>
+  <!-- 餐點 -->
+  <div class="productArea">
+    <div class="menuArea">
+      <div class="bandongArea">
+        <h3 id="riceBox">喜客油飯</h3>
+        <riceBoxCard />
+      </div>
+      <!-- <div class="guabaoArea">
+          <h3 id="guabao">刈包便當</h3>
+          <guabaoBandongCard />
+          <h3>刈包盒餐</h3>
+          <guabaoLunchboxCard />
+        </div> -->
+      <!-- <div class="budetCard1Area">
+        <h3 id="budetCard1">預算100元內</h3>
+        <budgetCard1 />
+      </div>
+      <div class="budetCard2Area">
+        <h3 id="budetCard2">預算125-220元</h3>
+        <budgetCard2 />
+      </div> -->
+    </div>
+
+    <!-- 可能喜歡 -->
+
+    <div class="section1">
+      <p class="sectionTitle">你可能會喜歡的中華美食！</p>
+      <div class="section1Cards">
+        <q-card class="section1Card" v-for="card in section1Cards" :key="card.imgSrc">
+          <div class="imgContainer">
+            <img :src="card.imgSrc" class="foodImg" :alt="card.title" />
+            <img :src="card.tagSrc" class="tag" alt="標籤" />
+          </div>
+          <q-card-section>
+            <div class="title" :class="`text-${card.titleColor}`">{{ card.title }}</div>
+            <div class="description">{{ card.description }}</div>
+          </q-card-section>
+          <q-card-section class="w-100 q-pa-md btns">
+            <q-btn
+              :outline="card.outline"
+              class="cardBtn"
+              :color="card.btnColor"
+              :to="card.href"
+              :class="{ disabled: card.disabled }"
+              @mouseover="toggleOutline(card)"
+              @mouseout="toggleOutline(card)">
+              {{ card.btnText }}
+            </q-btn>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+
+    <!-- 訂單說明 -->
+    <div class="orderDescription">
+      <orderType />
+    </div>
+  </div>
+  <lastSection />
+</template>
+
+<script setup>
+useHead({
+  title: '中華一餅 | 美味菜單',
+  meta: [
+    // Description Meta Tag
+    {
+      name: 'description',
+      content: '全台最好吃的潤餅，美味潤餅便當、招牌潤餅便當、素食潤餅便當、雙主餐便當，會議下午茶會議盒餐、貴賓盒餐、首席盒餐，中華一餅是您需要的會議餐盒外帶外送推薦。'
+    },
+    // Open Graph
+    {
+      property: 'og:title',
+      content: '中華一餅美味菜單'
+    },
+    {
+      property: 'og:description',
+      content: '全台最好吃的潤餅，美味潤餅便當、招牌潤餅便當、素食潤餅便當、雙主餐便當，會議下午茶會議盒餐、貴賓盒餐、首席盒餐，中華一餅是您需要的會議餐盒外帶外送推薦。'
+    },
+    {
+      property: 'og:image',
+      // content: '/index/0-pic-1.png' // 使用你的圖片路徑
+      content: '/favicon.ico' // 使用你的圖片路徑
+    },
+    {
+      property: 'og:image:alt',
+      content: '中華一餅圖片'
+    },
+    {
+      property: 'og:url',
+      content: 'https://www.chinabing.net/delicious'
+    },
+    {
+      property: 'og:type',
+      content: 'website'
+    }
+    // ...
+  ]
+})
+
+const section1Cards = ref([
+  {
+    imgSrc: '/delicious/dialog-2-pic-4.png',
+    tagSrc: '/delicious/bingTag.png',
+    title: '潤餅便當',
+    description: '中華一餅高人氣潤餅便當，傳承五十年好手藝結合創新技術，以潤餅為主角結合台灣在地好食材，是富有飽足感的美味餐盒！',
+    titleColor: 'pink-8',
+    btnColor: 'pink-8',
+    btnText: '查看潤餅便當菜單',
+    href: '/chinaBing',
+    disabled: false,
+    outline: true
+  },
+  {
+    imgSrc: '/delicious/douhua.png',
+    tagSrc: '/delicious/soyTag.png',
+    title: '豆花甜湯',
+    description: '甜品是讓人開讓人感覺到快樂與幸福，讓花豆囍事用最美好的味蕾感受，讓你天都能Sweet ! Sweet !',
+    titleColor: 'yellow-7',
+    btnColor: 'grey',
+    btnText: '盡請期待',
+    // href: '/budget',
+    disabled: true,
+    outline: true
+  }
+])
+
+const toggleOutline = card => {
+  card.outline = !card.outline
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/chinaBing.scss';
+</style>
